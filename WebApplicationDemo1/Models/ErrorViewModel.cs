206cc0cbb64f35d331a1ec3857605dc2ffd1ad74
Vanshita-Waghale <vanshitawaghale@gmail.com>
2025-09-22 21:54:59 +0530

    Added Sale and Purchase forms with CRUD operations

--- WebApplicationDemo1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplicationDemo1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- WebApplicationNew/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationNew.MvcApplication" Language="C#" %>
